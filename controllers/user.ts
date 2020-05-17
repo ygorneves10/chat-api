854b06bf768e252f5114fe76b8dfc4fd5e293c10
Ygor Neves <contato@ygorneves.com>
2020-05-16 21:24:48 -0300

    Add userController functions

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,5 +1,17 @@
 import { Sequelize, sequelize } from "./connect"
 
+interface User {
+    name: String
+    email: String
+    username: String
+}
+
+interface UserUpdate {
+    name?: String
+    email?: String
+    username?: String
+}
+
 const users = sequelize.define('users', {
     name: {
         type: Sequelize.TEXT,
@@ -21,4 +33,114 @@ const users = sequelize.define('users', {
     }
 });
 
-users.sync()
+const crud = {
+    create: function (attributes: User): Promise<User> {
+        return users.create(attributes)
+    },
+    read: function (email: String): Promise<User> {
+        return users.findOne({ where: { email } })
+    },
+    readAll: function (): Promise<Array<User>> {
+        return users.findAll()
+    },
+    update: function (id: Number, attributes: UserUpdate): Promise<Number> {
+        return users.update(attributes, { where: { id } })
+    },
+    delete: function (id: Number): Promise<Number> {
+        return users.destroy({ where: { id } })
+    }
+}
+
+export const userController = {
+    getAllUsers: function (req: any, res: any) {
+        crud.readAll().then(response => {
+            res.json({
+                success: true,
+                users: response
+            })
+        }).catch((error: Object) => {
+            res.json({
+                success: false,
+                error
+            })
+        })
+    },
+    getUser: function (req: any, res: any) {
+        const { email } = req.query
+
+        crud.read(email).then((response: Object) => {
+            res.json({
+                success: true,
+                user: response
+            })
+        }).catch((error: Object) => {
+            res.json({
+                success: false,
+                error
+            })
+        })
+    },
+    deleteUser: function (req: any, res: any) {
+        const { id } = req.params
+
+        crud.delete(id).then((response: Object) => {
+            res.json({
+                success: true,
+                user: response
+            })
+        }).catch((error: Object) => {
+            res.json({
+                success: false,
+                error
+            })
+        })
+    },
+    sendUser: function (req: any, res: any) {
+        const { id, email, name, username } = req.body
+
+        if (id) {
+            const updateAttributes: UserUpdate = {}
+
+            if (email) {
+                updateAttributes.email = email
+            }
+
+            if (name) {
+                updateAttributes.name = name
+            }
+
+            if (username) {
+                updateAttributes.username = username
+            }
+
+            return crud.update(id, updateAttributes).then((response: Object) => {
+                res.json({
+                    success: true,
+                    user: response
+                })
+            }).catch((error: Object) => {
+                res.json({
+                    success: false,
+                    error
+                })
+            })
+        }
+
+        const createAttributes: User = {
+            email, name, username
+        }
+
+        return crud.create(createAttributes).then(response => {
+            res.json({
+                success: true,
+                user: response
+            })
+        }).catch((error: Object) => {
+            res.json({
+                success: false,
+                error
+            })
+        })
+
+    }
+};
